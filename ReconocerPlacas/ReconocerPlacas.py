c7940a93b9471a59f4cd71db59fc8e7d0d224e8b
Andrea13Romero <57836527+Andrea13Romero@users.noreply.github.com>
2021-11-26 22:29:55 -0600

    Code Clean

--- ReconocerPlacas/ReconocerPlacas.py
+++ ReconocerPlacas/ReconocerPlacas.py
@@ -15,18 +15,12 @@ def connect():
         return conexion
     except Exception as e:
         print("ocurrió un error al conectar a SQL Server: ", e)
-
-def crearCarpeta():
-    if not os.path.exists(Datos):
-        os.makedirs(Datos)
-
 def SelectData(parametro):
     c1 = connect()
     try:
         with c1.cursor() as cursor:
             consulta = 'select nombre,paterno,materno,direccion,numero_placa  from Usuario join Placa on id_usario = Usuario_id_usario where numero_placa = ?'
             adr = parametro
-            # Podemos llamar muchas veces a .execute con datos distintos
             cursor.execute(consulta, parametro)     
             myresult = cursor.fetchall()
             if len(myresult)>0:
@@ -41,8 +35,6 @@ def InsertData(nombre, paterno, materno, direccion):
     try:
         with c1.cursor() as cursor:
             consulta = 'insert into usuario(nombre, paterno, materno, direccion) VALUES (?, ?, ?, ?)'
-            #adr = parametro
-            # Podemos llamar muchas veces a .execute con datos distintos
             cursor.execute(consulta,nombre, paterno, materno, direccion)   
         with c1.cursor() as cursor:
             lastdate = 'SELECT TOP 1 * FROM Usuario ORDER BY id_usario DESC'
@@ -58,8 +50,6 @@ def InsertPlate(Usuario_id_usario,plate):
     try:
         with c1.cursor() as cursor:
             consulta = 'insert into Placa(Usuario_id_usario, numero_placa) VALUES (?, ?)'
-            #adr = parametro
-            # Podemos llamar muchas veces a .execute con datos distintos
             cursor.execute(consulta,Usuario_id_usario, plate)     
     except Exception as e:
         print("Ocurrió un error al conectar a SQL Server: ", e)
@@ -71,10 +61,7 @@ def leer():
     gray = cv2.blur(gray,(3,3))
     canny = cv2.Canny(gray,150,200)
     canny = cv2.dilate(canny,None,iterations=1)
-    #cv2.imshow('Image',canny)
-    #_,cnts,_ = cv2.findContours(canny,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
     cnts,_ = cv2.findContours(canny,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-    #cv2.drawContours(image,cnts,-1,(0,255,0),2)
     for c in cnts:
         area = cv2.contourArea(c)
         x,y,w,h = cv2.boundingRect(c)
@@ -83,7 +70,6 @@ def leer():
   
         if len(approx)==4 and area>500:
             print('area=',area)
-            #cv2.drawContours(image,[approx],0,(0,255,0),3)
             aspect_ratio = float(w)/h
             if aspect_ratio>0.1:
                 placa = gray[y:y+h,x:x+w]
@@ -104,26 +90,6 @@ def leer():
         
 x1, y1 = 190, 80
 x2, y2 = 490, 380
-def captura():
-    cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-    count = 0
-    while True:
-        ret, frame = cap.read()
-        if ret == False:  break
-        imAux = frame.copy()
-        cv2.rectangle(frame,(x1,y1),(x2,y2),(255,0,0),2)
-        objeto = imAux[y1:y2,x1:x2]
-        objeto = imutils.resize(objeto, width=300)
-        cv2.imshow('frame',frame)
-        #k = cv2.waitKey(1)
-        if cv2.waitKey(1) == 27:
-            break
-    cv2.imwrite('temporal'+'/objeto_{}.jpg'.format(count),objeto)
-   
-    cap.release()
-    cv2.destroyAllWindows()
-
-
 def menu():
     try:
         opc = int(input("1.Crear usuario. \n2.Detectar Placa. \n3.salir"))
@@ -131,10 +97,7 @@ def menu():
     except Exception as e:
       print("Ingresa una opcion valida", e)
 def main():
-    #captura()
-    #a = connect()
-    #interfaz()
-    #leer()
+
     opc = menu()
     while(opc!=3):
         if opc==1:
@@ -143,7 +106,6 @@ def main():
             materno = input("Materno: ")
             direccion = input("Dirección: ")
             InsertData(nombre, paterno, materno, direccion)
-            #objeto Create Folder
             opc = menu()
         elif opc==2:
             leer()
